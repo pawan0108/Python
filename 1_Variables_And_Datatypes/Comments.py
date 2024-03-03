a45219e71368655cbb1958aebfee2df435811310
pawan0108 <pawanmaurya8931@gmail.com>
2024-03-03 21:38:44 +0530

    This is folder of Variables and Datatype

---
+++ 1_Variables_And_Datatypes/Comments.py
@@ -0,0 +1,10 @@
+# this is single line Comments
+
+print("Hello World !")
+
+"""
+This is Multi Linr comments
+here you can write any thing this is not Executes
+"""
+
+print("Multi Line Cooments")
---
+++ 1_Variables_And_Datatypes/Variables.py
@@ -0,0 +1,15 @@
+# Variables Declaration.
+
+a = 6
+b = 7
+print(a)
+print(b)
+
+# Casting
+
+d = str("Ayush") 
+e = int(45)
+g = float(65.7)
+print(d)
+print(e)
+print(g)
